353de58bf00980678ff7f852ce3a0349006c372a
sirdiego456 <diegomachaca20@gmail.com>
2025-04-16 07:28:35 -0500

    Update p02_DC.py

--- Pilas_y_Colas/p02_DC.py
+++ Pilas_y_Colas/p02_DC.py
@@ -6,13 +6,11 @@ class NodoPila:
 class Pila:
     def __init__(self):
         self.top = None
-        self.size = 0
 
     def push(self, valor):
         nodo = NodoPila(valor)
         nodo.next = self.top
         self.top = nodo
-        self.size += 1
     
     def print(self):
         nodo = self.top
@@ -20,12 +18,13 @@ class Pila:
             print(nodo.valor, end = (" -> " if nodo.next is not None else "\n"))
             nodo = nodo.next
 
-p= Pila()
+p = Pila()
 print("Ingrese elementos a la pila. Escriba 'fin' para terminar.")
 while True:
     val = input("Ingrese un valor: ")
     if val.lower() == 'fin':
         break
     p.push(int(val))
+
 print("Contenido de la pila: ")
 p.print()
