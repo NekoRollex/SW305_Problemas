abd082d8fc3396a9f75e7f56db5496c376e41821
sirdiego456 <diegomachaca20@gmail.com>
2025-04-16 06:02:07 -0500

    Add files via upload

---
+++ Listas_Enlazadas/p01_DC.py
@@ -0,0 +1,32 @@
+class nodolista:
+    def __init__(self, valor):
+        self.valor= valor
+        self.puntero= None
+class ListaEnlazada:
+    def __init__(self):
+        self.pinicial= None
+    def insertar_final(self, valor):
+        nuevo= nodolista(valor)
+        if self.pinicial== None:
+            self.pinicial= nuevo
+        else:
+            actual= self.pinicial
+            while actual.puntero!= None:
+                actual= actual.puntero
+            actual.puntero= nuevo
+    #def mostrar_lista(self):
+    #    actual= self.pinicial
+    #    while actual!= None:
+    #        print(actual.valor, end=", ")
+    #        actual= actual.puntero
+    #    print("None")
+lista= ListaEnlazada()
+
+print("Agregar al final de la lista enlazada. Escriba 'fin' para terminar.")
+while True:
+    val= input("Ingrese un valor: ")
+    if val.lower()== "fin":
+        break
+    lista.insertar_final(val)
+#print("Lista enlazada:")
+#lista.mostrar_lista()
