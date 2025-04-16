b823b47c457ea4bcd65542543889d197a32d62c8
sirdiego456 <diegomachaca20@gmail.com>
2025-04-16 07:30:01 -0500

    Update p03_DC.py

--- Pilas_y_Colas/p03_DC.py
+++ Pilas_y_Colas/p03_DC.py
@@ -6,19 +6,16 @@ class NodoPila:
 class Pila:
     def __init__(self):
         self.top = None
-        self.size = 0
 
     def push(self, valor):
         nodo = NodoPila(valor)
         nodo.next = self.top
         self.top = nodo
-        self.size += 1
     
     def pop(self):
         if not self.is_empty():
             nodo = self.top
             self.top = nodo.next
-            self.size -= 1
 
     def peek(self):
         if self.top is not None:
@@ -26,7 +23,7 @@ class Pila:
         return None
 
     def is_empty(self):
-        return self.size == 0
+        return self.top is None
 
     def print(self):
         nodo = self.top
