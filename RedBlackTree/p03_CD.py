d0b1f765a9e420f93d35ec9c928ad4e7b60f45d5
CrhistianDL <crhistian.dionicio.l@uni.pe>
2025-04-30 01:49:38 -0500

    Ejercicios de RedBlackTree

---
+++ RedBlackTree/p03_CD.py
@@ -0,0 +1,144 @@
+class NodoRB:
+    def __init__(self, value, color='red'):
+        self.value = value
+        self.color = color
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def grandparent(self):
+        if self.parent is None:
+            return None
+        return self.parent.parent
+
+    def sibling(self):
+        if self.parent is None:
+            return None
+        if self == self.parent.left:
+            return self.parent.right
+        return self.parent.left
+
+    def uncle(self):
+        if self.parent is None:
+            return None
+        return self.parent.sibling()
+
+class ArbolRB:
+    def __init__(self):
+        self.root = None
+
+    def insert_fix(self, new_node):
+          # While there are two continuous red nodes, we need to fix the RB tree
+        while new_node.parent and new_node.parent.color == 'red':
+            # If the parent is left child of grandparent
+            if new_node.parent == new_node.grandparent().left:
+                uncle = new_node.uncle()
+                if uncle and uncle.color == 'red':
+                    new_node.parent.color = 'black'
+                    uncle.color = 'black'
+                    new_node.grandparent().color = 'red'
+                    new_node = new_node.grandparent()
+                else:
+                    if new_node == new_node.parent.right:
+                        new_node = new_node.parent
+                        self.rotate_left(new_node)
+                    new_node.parent.color = 'black'
+                    new_node.grandparent().color = 'red'
+                    self.rotate_right(new_node.grandparent())
+            # If the parent is right child of grandparent
+            else:
+                uncle = new_node.uncle()
+                if uncle and uncle.color == 'red':
+                    new_node.parent.color = 'black'
+                    uncle.color = 'black'
+                    new_node.grandparent().color = 'red'
+                    new_node = new_node.grandparent()
+                else:
+                    if new_node == new_node.parent.left:
+                        new_node = new_node.parent
+                        self.rotate_right(new_node)
+                    new_node.parent.color = 'black'
+                    new_node.grandparent().color = 'red'
+                    self.rotate_left(new_node.grandparent())
+        self.root.color = 'black'
+
+    def insert(self, value):
+        new_node = NodoRB(value)
+        if self.root is None:
+            self.root = new_node
+        else:
+            curr_node = self.root
+            while True:
+                if value < curr_node.value:
+                    if curr_node.left is None:
+                        curr_node.left = new_node
+                        new_node.parent = curr_node
+                        break
+                    else:
+                        curr_node = curr_node.left
+                else:
+                    if curr_node.right is None:
+                        curr_node.right = new_node
+                        new_node.parent = curr_node
+                        break
+                    else:
+                        curr_node = curr_node.right
+        self.insert_fix(new_node)
+
+    def rotate_left(self, node):
+        right_child = node.right
+        node.right = right_child.left
+
+        if right_child.left is not None:
+            right_child.left.parent = node
+
+        right_child.parent = node.parent
+
+        if node.parent is None:
+            self.root = right_child
+        elif node == node.parent.left:
+            node.parent.left = right_child
+        else:
+            node.parent.right = right_child
+
+        right_child.left = node
+        node.parent = right_child
+
+    def rotate_right(self, node):
+        left_child = node.left
+        node.left = left_child.right
+
+        if left_child.right is not None:
+            left_child.right.parent = node
+
+        left_child.parent = node.parent
+
+        if node.parent is None:
+            self.root = left_child
+        elif node == node.parent.right:
+            node.parent.right = left_child
+        else:
+            node.parent.left = left_child
+
+        left_child.right = node
+        node.parent = left_child
+
+    def imprimir_colores(self, nodo=None):
+        if nodo is None:
+            nodo = self.root
+        padre_valor = nodo.parent.value if nodo.parent else "Ninguno"
+        print(f"Valor: {nodo.value}, Color: {nodo.color}, Padre: {padre_valor}")
+        if nodo.left:
+            self.imprimir_colores(nodo.left)
+        if nodo.right:
+            self.imprimir_colores(nodo.right)
+
+
+arbol = ArbolRB()
+valores = [10, 20, 30, 15, 25, 5]
+
+for valor in valores:
+    arbol.insert(valor)
+
+
+arbol.imprimir_colores()
