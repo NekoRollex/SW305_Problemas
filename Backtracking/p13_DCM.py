a131f26fbe265046eee8ae83da2f007c1146fa26
sirdiego456 <diegomachaca20@gmail.com>
2025-05-25 22:16:54 -0500

    Add files via upload

---
+++ Backtracking/p13_DCM.py
@@ -0,0 +1,29 @@
+def subset_sum(num, objetivo, indice=0, suma_actual=0, sub_actual=[]):
+    if suma_actual == objetivo:
+        print("Subconjunto encontrado:", sub_actual)
+        return True
+    if suma_actual > objetivo or indice == len(num):
+        return False
+
+    sub_actual.append(num[indice])
+    if subset_sum(num, objetivo, indice + 1, suma_actual + num[indice], sub_actual):
+        return True
+    sub_actual.pop()
+
+    if subset_sum(num, objetivo, indice + 1, suma_actual, sub_actual):
+        return True
+
+    return False
+
+conjunto = []
+n = int(input("numero de elementos del conjunto: "))
+for i in range(n):
+    num = int(input(f"Ingrese el elemento {i+1}: "))
+    conjunto.append(num)
+
+objetivo = int(input("Ingresa el valor objetivo a calcular: "))
+
+existe = subset_sum(conjunto, objetivo)
+
+if not existe:
+    print(f"No existe un subconjunto cuya suma sea {objetivo}")
