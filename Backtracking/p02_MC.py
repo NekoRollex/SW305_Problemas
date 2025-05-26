4dae14ba3925f024feb77457cbbb19ee4c7ec958
MijailUNI <mijail.cuadros.s@uni.pe>
2025-05-26 11:23:14 -0500

    Add files via upload

---
+++ Backtracking/p02_MC.py
@@ -0,0 +1,68 @@
+class Nodo:
+    def __init__(self, val):
+        self.valor = val
+        self.izquierdo = None
+        self.derecho = None
+        self.padre = None
+
+class ArbolBinario:
+    def __init__(self):
+        self.raiz = None
+    
+    def insertar(self, val):
+        n = Nodo(val)
+        if self.raiz != None:
+            paux = self.raiz
+            while paux != None:
+                ppadre = paux
+                if val < paux.valor:
+                    paux = paux.izquierdo
+                else:
+                    paux = paux.derecho
+            n.padre = ppadre
+            if val < ppadre.valor:
+                ppadre.izquierdo = n
+            else:
+                ppadre.derecho = n
+        else:
+            self.raiz = n
+
+def Backtracking(nodo, suma, ruta, rutasencontradas, sumadeseada):
+        if not nodo:
+            return
+
+        suma += nodo.valor
+        ruta.append(nodo.valor)
+
+        if not nodo.izquierdo and not nodo.derecho:
+            if suma == sumadeseada:
+                rutasencontradas.append(list(ruta)) 
+
+        Backtracking(nodo.izquierdo, suma, ruta, rutasencontradas, sumadeseada)
+        Backtracking(nodo.derecho, suma, ruta, rutasencontradas, sumadeseada)
+
+        ruta.pop()
+
+def EncontrarRutas(arbol):
+    suma = 0
+    sumadeseada = int(input("Ingrese la suma que desea buscar: "))
+    ruta = []
+    rutasencontradas = []
+
+    Backtracking(arbol.raiz, suma, ruta, rutasencontradas, sumadeseada)
+    return rutasencontradas
+
+arbol = ArbolBinario()
+
+print("Ingrese el arbol binario")
+print("Si quiere salir ingrese 0")
+
+valor = 1
+while valor != 0:
+    valor = int(input("Ingrese un elemento: "))
+    if valor != 0:
+        arbol.insertar(valor)
+
+rutas = EncontrarRutas(arbol)
+print("Las rutas encontradas son: ")
+print(rutas)
